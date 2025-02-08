f87725897b738b84fbfcc4f621e2296e2d0bd7e3
Viktor <konvit16@gmail.com>
2025-02-08 03:52:18 -0800

    ios AppStateProvider убрал логику

--- iosApp/iosApp/WebSocketBackgroundTask.swift
+++ iosApp/iosApp/WebSocketBackgroundTask.swift
@@ -1,29 +1,30 @@
-import platform.Foundation.NSURLSession
-import platform.Foundation.NSURLSessionConfiguration
-import platform.BackgroundTasks.BGTaskScheduler
-
-class WebSocketBackgroundTask {
-
-    static let identifier = "com.martin.websocket.ping"
-
-    func schedule() {
-        let request = BGProcessingTaskRequest(identifier: WebSocketBackgroundTask.identifier)
-        request.requiresNetworkConnectivity = true
-        request.requiresExternalPower = false
-
-        do {
-            try BGTaskScheduler.shared.submit(request)
-        } catch {
-            print("Could not schedule WebSocket ping task: \(error)")
-        }
-    }
-
-    func handle(task: BGTask) {
-        schedule()
-
-        let webSocketManager = WebSocketManager.shared
-        webSocketManager.send("ping")
-
-        task.setTaskCompleted(success: true)
-    }
-}
+//import Foundation
+//import platform.Foundation.NSURLSession
+//import platform.Foundation.NSURLSessionConfiguration
+//import platform.BackgroundTasks.BGTaskScheduler
+//
+//class WebSocketBackgroundTask {
+//
+//    static let identifier = "com.martin.websocket.ping"
+//
+//    func schedule() {
+//        let request = BGProcessingTaskRequest(identifier: WebSocketBackgroundTask.identifier)
+//        request.requiresNetworkConnectivity = true
+//        request.requiresExternalPower = false
+//
+//        do {
+//            try BGTaskScheduler.shared.submit(request)
+//        } catch {
+//            print("Could not schedule WebSocket ping task: \(error)")
+//        }
+//    }
+//
+//    func handle(task: BGTask) {
+//        schedule()
+//
+//        let webSocketManager = WebSocketManager.shared
+//        webSocketManager.send("ping")
+//
+//        task.setTaskCompleted(success: true)
+//    }
+//}
--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -1,21 +1,23 @@
 import SwiftUI
 import ComposeApp
+import BackgroundTasks
+
 
 @main
 struct iOSApp: App {
     init() {
         KoinKt.doInitKoin()
 
-        BGTaskScheduler.shared.register(forTaskWithIdentifier: WebSocketBackgroundTask.identifier, using: nil) { task in
-            WebSocketBackgroundTask().handle(task: task)
-        }
+//        BGTaskScheduler.shared.register(forTaskWithIdentifier: WebSocketBackgroundTask.identifier, using: nil) { task in
+//            WebSocketBackgroundTask().handle(task: task)
+//        }
     }
 
     var body: some Scene {
         WindowGroup {
             ContentView()
             .onAppear {
-                WebSocketBackgroundTask().schedule()
+                //WebSocketBackgroundTask().schedule()
             }
         }
     }
