59cc4f7e403f68fdf4cbd51724ad58e8c5307dcb
Viktor <konvit16@gmail.com>
2025-02-20 08:27:35 -0800

    fix

--- composeApp/src/iosMain/kotlin/com/jetbrains/kmpapp/feature/sockets/WebSocketWorker.kt
+++ composeApp/src/iosMain/kotlin/com/jetbrains/kmpapp/feature/sockets/WebSocketWorker.kt
@@ -1,32 +1,36 @@
 package com.jetbrains.kmpapp.feature.sockets
 
-//import com.jetbrains.kmpapp.data.sockets.WebSocketService
-//
-//class WebSocketWorker {
-//    private val webSocketService = IOSWebSocketService() // Твой сервис, как AndroidWebSocketService
-//    private var backgroundTaskId: UIBackgroundTaskIdentifier = UIBackgroundTaskIdentifier.invalid
-//
-//    init {
-//        startBackgroundTask()
-//    }
-//
-//    private fun startBackgroundTask() {
-//        backgroundTaskId = UIApplication.shared.beginBackgroundTask {
-//            // Если задача завершилась, сообщаем системе, что работа окончена
-//            UIApplication.shared.endBackgroundTask(backgroundTaskId)
-//            backgroundTaskId = UIBackgroundTaskIdentifier.invalid
-//        }
-//
-//        // Инициализация WebSocket-сервиса
-//        println("WebSocketWorker: WebSocketService initialized in background.")
-//    }
-//
-//    fun stopBackgroundTask() {
-//        if (backgroundTaskId != UIBackgroundTaskIdentifier.invalid) {
-//            UIApplication.shared.endBackgroundTask(backgroundTaskId)
-//            backgroundTaskId = UIBackgroundTaskIdentifier.invalid
-//        }
-//    }
-//
-//    fun getWebSocketService(): WebSocketService = webSocketService
-//}
+import com.jetbrains.kmpapp.data.sockets.WebSocketService
+import platform.UIKit.UIApplication
+import platform.UIKit.UIBackgroundTaskIdentifier
+import platform.UIKit.beginBackgroundTask
+import platform.UIKit.endBackgroundTask
+
+class WebSocketWorker {
+    private val webSocketService = IOSWebSocketService() // Твой сервис, как AndroidWebSocketService
+    private var backgroundTaskId: UIBackgroundTaskIdentifier = UIBackgroundTaskIdentifier.invalid
+
+    init {
+        startBackgroundTask()
+    }
+
+    private fun startBackgroundTask() {
+        backgroundTaskId = UIApplication.shared.beginBackgroundTask {
+            // Если задача завершилась, сообщаем системе, что работа окончена
+            UIApplication.shared.endBackgroundTask(backgroundTaskId)
+            backgroundTaskId = UIBackgroundTaskIdentifier.invalid
+        }
+
+        // Инициализация WebSocket-сервиса
+        println("WebSocketWorker: WebSocketService initialized in background.")
+    }
+
+    fun stopBackgroundTask() {
+        if (backgroundTaskId != UIBackgroundTaskIdentifier.invalid) {
+            UIApplication.shared.endBackgroundTask(backgroundTaskId)
+            backgroundTaskId = UIBackgroundTaskIdentifier.invalid
+        }
+    }
+
+    fun getWebSocketService(): WebSocketService = webSocketService
+}
--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -1,28 +1,3 @@
-//import SwiftUI
-//import ComposeApp
-//import BackgroundTasks
-//
-//
-//@main
-//struct iOSApp: App {
-//    init() {
-//        KoinKt.doInitKoin()
-//
-//        BGTaskScheduler.shared.register(forTaskWithIdentifier: WebSocketBackgroundTask.identifier, using: nil) { task in
-//            WebSocketBackgroundTask().handle(task: task)
-//        }
-//    }
-//
-//    var body: some Scene {
-//        WindowGroup {
-//            ContentView()
-//            .onAppear {
-//                WebSocketBackgroundTask().schedule()
-//            }
-//        }
-//    }
-//}
-
 import ComposeApp
 import SwiftUI
 import BackgroundTasks
